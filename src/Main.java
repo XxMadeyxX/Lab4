7549697a63f2d631a0016ff0c098ee642d838f2f
XxMadeyxX <lysaght.m@ufl.edu>
2017-09-26 13:47:39 -0400

    simple program that adds 2 numbers together.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+    Scanner scnr = new Scanner(System.in);
+
+    System.out.println("Welcome to this Computer!");
+    System.out.println("This program is designed to complete simple addition of 2 numbers.");
+    System.out.println("");
+    System.out.println("Please enter you first number: ");
+    int number1 = scnr.nextInt();
+    System.out.println("Please enter your second number: ");
+    int number2 = scnr.nextInt();
+
+    int result = 0;
+    result = (number1 + number2);
+    System.out.println("Your result is " + result);
+    }
+}
